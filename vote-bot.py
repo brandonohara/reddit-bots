cd13e625823b12be5226da0addd85b9efbda87c9
Brandon O'Hara <brandon@brandonohara.com>
2015-03-17 11:39:24 -0600

    vote-bot.py cleanup

--- vote-bot.py
+++ vote-bot.py
@@ -7,13 +7,13 @@ import praw
 __author__ = "Brandon O'Hara"
 __version__ = '1.0.0'
 
-class KriegerBot(object):
+class VotingBot(object):
 	def __init__(self, upvote, target):
+		global __version__
 		self.username = 'dr-krieger-bot'
-		self.version = '1.0.0'
 		self.password = getpass.getpass('Password for <%s>:' %self.username)
 		
-		self.r = praw.Reddit(user_agent= self.username + ' v' + self.version)
+		self.r = praw.Reddit(user_agent= self.username + ' v' + __version__)
 		self.r.login(self.username,self.password)
 		
 		self.upvoted = 0
@@ -23,15 +23,14 @@ class KriegerBot(object):
 		self.redditor = self.r.get_redditor(target, fetch=False)
 		
 	def run(self, limit):
-		print('Limit: %d', limit)
 		for post in self.redditor.get_overview(limit = limit):
 			try:
 				if self.upvote:
-					#post.upvote()
+					post.upvote()
 					print "upvote"
 					self.upvoted += 1
 				else:
-					#post.downvote()
+					post.downvote()
 					print "downvote"
 					self.downvoted += 1
 			except:
@@ -40,8 +39,8 @@ class KriegerBot(object):
 	
 
 def main(argv):
-	limit = 10
-	help = 'bot.py -h -d-u <reddit-username> -l <limit>'
+	limit = 10 #default to ten
+	help = 'bot.py -h -du <reddit-username> -l <limit>'
 	try:
 		opts, args = getopt.getopt(argv,"d:u:l:")
 	except getopt.GetoptError:
@@ -60,13 +59,13 @@ def main(argv):
 		elif opt in ("-l", "--limit"):
 			limit = int(arg)
         	
-	bot = KriegerBot(upvote, target)
 	try:
+		bot = VotingBot(upvote, target)
 		bot.run(limit)
 	except:
 		raise
 	finally:
-		print('All done! Upvoted %d, Downvoted %d!' %(bot.upvoted, bot.downvoted))
+		print('Upvoted %d, Downvoted %d!' %(bot.upvoted, bot.downvoted))
 	
 if __name__ == '__main__':
 	target = sys.argv[1:]
